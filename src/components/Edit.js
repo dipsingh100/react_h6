83146f625a49b4eb154914de4d06e497a08ccd44
dipsingh100 <angelicsmile701@gmail.com>
2023-08-02 23:13:03 +0530

    made responsive

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,9 +1,10 @@
 import React, { useContext , useState} from 'react'
 import "../css/students.css";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import dataContext from '../Data';
 
 const Edit = () => {
+  const navigate = useNavigate()
   const id = parseInt(useParams().id)
   const [data, setdata] = useContext(dataContext);
   const [name, setName] = useState(data[id].name)
@@ -19,31 +20,35 @@ const Edit = () => {
     }
     data[id] = newStudent
     setdata(data)
+    navigate('/students')
   }
   return (
     <>
-      <form className="form-container">
-        <div className="inp-container">
-          <input type="text" name="name" value={name} required onChange={(e)=>setName(e.target.value)}/>
-          <label>Name</label>
+    <div className="container">
+      <h1>Update Student</h1>
+        <form className="form-container">
+          <div className="inp-container">
+            <input type="text" name="name" value={name} required onChange={(e)=>setName(e.target.value)}/>
+            <label>Name</label>
+          </div>
+          <div className="inp-container">
+            <input type="text" name="age" value={age} required onChange={(e)=>setAge(e.target.value)}/>
+            <label>Age</label>
+          </div>
+          <div className="inp-container">
+            <input type="text" name="course" value={course} required onChange={(e)=>setCourse(e.target.value)}/>
+            <label>Course</label>
+          </div>
+          <div className="inp-container">
+            <input type="text" name="batch" value={batch} required onChange={(e)=>setBatch(e.target.value)}/>
+            <label>Batch</label>
+          </div>
+        </form>
+        <div className="buttons">
+          <button onClick={()=>navigate('/students')}>Cancel</button>
+          <button onClick={handleSubmit}>Update</button>
         </div>
-        <div className="inp-container">
-          <input type="text" name="age" value={age} required onChange={(e)=>setAge(e.target.value)}/>
-          <label>Age</label>
-        </div>
-        <div className="inp-container">
-          <input type="text" name="course" value={course} required onChange={(e)=>setCourse(e.target.value)}/>
-          <label>Course</label>
-        </div>
-        <div className="inp-container">
-          <input type="text" name="batch" value={batch} required onChange={(e)=>setBatch(e.target.value)}/>
-          <label>Batch</label>
-        </div>
-      </form>
-      <div className="buttons">
-        <button>Cancel</button>
-        <Link to={'/students'}><button onClick={handleSubmit}>Update</button></Link>
-      </div>
+    </div>
     </>
   )
 }
--- src/components/StudentInfo.js
+++ src/components/StudentInfo.js
@@ -1,9 +1,10 @@
 import React, { useContext, useState } from "react";
 import "../css/students.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import dataContext from "../Data";
 
 const StudentInfo = () => {
+  const navigate = useNavigate()
   const [data, setdata] = useContext(dataContext);
   const [name, setName] = useState("")
   const [age, setAge] = useState("")
@@ -18,30 +19,34 @@ const StudentInfo = () => {
     }
     data.push(newStudent)
     setdata(data)
+    navigate('/students')
   }
   return (
     <>
-      <form className="form-container">
-        <div className="inp-container">
-          <input type="text" name="name" value={name} required onChange={(e) => setName(e.target.value)} />
-          <label>Name</label>
+      <div className="container">
+        <h1 >Add New Student</h1>
+        <form className="form-container">
+          <div className="inp-container">
+            <input type="text" name="name" value={name} required onChange={(e) => setName(e.target.value)} />
+            <label>Name</label>
+          </div>
+          <div className="inp-container">
+            <input type="text" name="age" value={age} required onChange={(e) => setAge(e.target.value)} />
+            <label>Age</label>
+          </div>
+          <div className="inp-container">
+            <input type="text" name="course" value={course} required onChange={(e) => setCourse(e.target.value)} />
+            <label>Course</label>
+          </div>
+          <div className="inp-container">
+            <input type="text" name="batch" value={batch} required onChange={(e) => setBranch(e.target.value)} />
+            <label>Batch</label>
+          </div>
+        </form>
+        <div className="buttons">
+          <button onClick={() => navigate('/students')}>Cancel</button>
+          <button onClick={handleSubmit}>Submit</button>
         </div>
-        <div className="inp-container">
-          <input type="text" name="age" value={age} required onChange={(e) => setAge(e.target.value)} />
-          <label>Age</label>
-        </div>
-        <div className="inp-container">
-          <input type="text" name="course" value={course} required onChange={(e) => setCourse(e.target.value)} />
-          <label>Course</label>
-        </div>
-        <div className="inp-container">
-          <input type="text" name="batch" value={batch} required onChange={(e) => setBranch(e.target.value)} />
-          <label>Batch</label>
-        </div>
-      </form>
-      <div className="buttons">
-        <button>Cancel</button>
-        <Link to={'/students'}><button onClick={handleSubmit}>Submit</button></Link>
       </div>
     </>
   );
